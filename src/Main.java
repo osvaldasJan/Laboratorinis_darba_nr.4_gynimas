0a1d1e9dcc4b71b6c1b86f4b9f0e3b364c22f66b
osvaldasJan <jankauskas.osvaldas@gmail.com>
2018-11-01 15:25:20 +0200

    -

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -26,19 +28,99 @@ public class Main {
         //REKOMENDACIJA:
         //kurti naują projektą.*/
 
-         Automobilis hondaCivic = arrayListToCar(readingValueFromFile("auto1.txt"));
-         Automobilis audi100 = arrayListToCar(readingValueFromFile("auto2.txt"));
-         Automobilis hyundai20 = arrayListToCar(readingValueFromFile("auto3.txt"));
-         Automobilis yaris = arrayListToCar(readingValueFromFile("auto4.txt"));
-         Automobilis octavia = arrayListToCar(readingValueFromFile("auto5.txt"));
-
-        System.out.println(octavia.isElektromobilis());
-
+        Automobilis hondaCivic = arrayListToCar(readingValueFromFile("auto1.txt"));
+        Automobilis audi100 = arrayListToCar(readingValueFromFile("auto2.txt"));
+        Automobilis hyundai20 = arrayListToCar(readingValueFromFile("auto3.txt"));
+        Automobilis yaris = arrayListToCar(readingValueFromFile("auto4.txt"));
+        Automobilis octavia = arrayListToCar(readingValueFromFile("auto5.txt"));
 
         atspausdintiAutomobiliuSarasoAntraste();
         atspausdintiAutomobiliuSarasa(
                 pridetiAutomobilisISarasa(hondaCivic, audi100, hyundai20, yaris, octavia));
         atspausdintiAutomobiliuSarasoPoraste();
+
+        JFrame frame = new JFrame("JFrame Example");
+        String[] columnNames = {
+                "Marke ",
+                "Modelis ",
+                "Spalva ",
+                "Metai ",
+                "Duru skaicius",
+                "Rida",
+                "Varomas elektra "
+        };
+
+
+        Object[][] data = {
+                {
+                        hondaCivic.getMarke(),
+                        hondaCivic.getModelis(),
+                        hondaCivic.getSpalva(),
+                        hondaCivic.getPagaminimoMetai(),
+                        hondaCivic.getDuruSkaicius(),
+                        hondaCivic.getRida(),
+                        hondaCivic.isElektromobilis()
+                },
+                {
+                        audi100.getMarke(),
+                        audi100.getModelis(),
+                        audi100.getSpalva(),
+                        audi100.getPagaminimoMetai(),
+                        audi100.getDuruSkaicius(),
+                        audi100.getRida(),
+                        audi100.isElektromobilis()
+                },
+                {
+                        octavia.getMarke(),
+                        octavia.getModelis(),
+                        octavia.getSpalva(),
+                        octavia.getPagaminimoMetai(),
+                        octavia.getDuruSkaicius(),
+                        octavia.getRida(),
+                        octavia.isElektromobilis()
+                },
+                {
+                        hyundai20.getMarke(),
+                        hyundai20.getModelis(),
+                        hyundai20.getSpalva(),
+                        hyundai20.getPagaminimoMetai(),
+                        hyundai20.getDuruSkaicius(),
+                        hyundai20.getRida(),
+                        hyundai20.isElektromobilis()
+                },
+                {
+                        yaris.getMarke(),
+                        yaris.getModelis(),
+                        yaris.getSpalva(),
+                        yaris.getPagaminimoMetai(),
+                        yaris.getDuruSkaicius(),
+                        yaris.getRida(),
+                        yaris.isElektromobilis()
+                }
+        };
+
+        JTable table = new JTable(data, columnNames);
+        JScrollPane spTable = new JScrollPane(table);
+        JPanel panel = new JPanel();
+        panel.setLayout(new
+
+                FlowLayout());
+
+        JLabel label = new JLabel("This is a label!");
+
+        JButton button = new JButton();
+        button.setText("Press me");
+
+        panel.add(label);
+        panel.add(button);
+        panel.add(spTable);
+
+        frame.add(panel);
+        frame.setSize(500, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 
     public static ArrayList<Object> readingValueFromFile(String filePath) throws FileNotFoundException {
@@ -64,16 +146,16 @@ public class Main {
         arrayFromFile.forEach(System.out::println);
     }
 
-    public static Automobilis arrayListToCar(ArrayList <Object> arrayList) {
-                Automobilis car = new Automobilis(
-                        String.valueOf(arrayList.get(0)),
-                        String.valueOf(arrayList.get(1)),
-                        String.valueOf(arrayList.get(2)),
-                        Integer.valueOf(String.valueOf(arrayList.get(3))),
-                        Integer.valueOf(String.valueOf(arrayList.get(4))),
-                        Integer.valueOf(String.valueOf(arrayList.get(5))),
-                        Boolean.valueOf(String.valueOf(arrayList.get(6)))
-                );
+    public static Automobilis arrayListToCar(ArrayList<Object> arrayList) {
+        Automobilis car = new Automobilis(
+                String.valueOf(arrayList.get(0)),
+                String.valueOf(arrayList.get(1)),
+                String.valueOf(arrayList.get(2)),
+                Integer.valueOf(String.valueOf(arrayList.get(3))),
+                Integer.valueOf(String.valueOf(arrayList.get(4))),
+                Integer.valueOf(String.valueOf(arrayList.get(5))),
+                Boolean.valueOf(String.valueOf(arrayList.get(6)))
+        );
         return car;
     }
 
@@ -97,7 +179,7 @@ public class Main {
                     automobilis.get(i).getRida(),
                     automobilis.get(i).isElektromobilis()
             );
-        }//
+        }
     }
 
     public static void atspausdintiAutomobiliuSarasoAntraste() {
